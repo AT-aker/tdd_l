dff14a098e1e46f50cc1c02614720249aad744cc
Anatolii Kukoviakin <a.kukoviakin@gmail.com>
2022-06-06 03:18:58 +0300

    FT chengd, used unittest

--- superlists/tests/functional_test.py
+++ superlists/tests/functional_test.py
@@ -1,7 +1,35 @@
 from selenium import webdriver
+import unittest
 
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    """test new user"""
 
-assert 'Congratulations!' in browser.title
+    def setUp(self) -> None:
+        '''install'''
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self) -> None:
+        '''quit'''
+        self.browser.quit()
+
+    def test_can_start_a_list_and_reteieve_it_later(self):
+        '''Test: we can start the list and give up this list later'''
+        # evaluation of the application at
+        #  the address of its website
+        self.browser.get('http://localhost:8000')
+
+        # Check 'to-do' in title main page 
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('End the Test')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
+
+# We have some user who need to input the value into check box
+# and this value add to list 'To-Do'
+# this user input "clean the floor"
+# and he get "1. Clean the floor"
+# user input second value and take two values in list
+# and his list stored in new Url - site messeged him about 
