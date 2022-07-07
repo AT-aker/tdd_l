587314de1ae58980ffa389b73401483ddde0fc1c
Anatolii Kukoviakin <a.kukoviakin@gmail.com>
2022-07-07 23:13:31 +0300

    First step FT for layout + styling

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -127,5 +127,26 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('clean the floor', page_text)
         self.assertIn('Buy milk', page_text)
 
-# if __name__ == '__main__':
-#     unittest.main(warnings='ignore')
+    def test_layout_and_styling(self):
+        '''test: layout and styling '''
+        # Bill open home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # He notices that the input field is neatly centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        # he create new list and see input field is neatly centered
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        self.assertAlmostEqual(
+            inputbox.location["x"] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
